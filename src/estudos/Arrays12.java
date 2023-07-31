aff6f9d9b945e59c1c16cff61555b9be6da0ddb6
arthurNdev <arthurnina.dev@gmail.com>
2023-07-31 16:48:08 -0300

    Estudos Java

---
+++ src/estudos/Arrays12.java
@@ -0,0 +1,40 @@
+package estudos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Arrays12 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos alunos serão digitados? ");
+        int n = sc.nextInt();
+
+        String[] names = new String[n];
+        double[] score1 = new double[n];
+        double[] score2 = new double[n];
+
+        for (int i=0; i<n; i++) {
+            System.out.println("Digite nome, primeira e segunda nota do "
+                    + (i+1) + "o aluno:");
+            sc.nextLine();
+            names[i] = sc.nextLine();
+            score1[i] = sc.nextDouble();
+            score2[i] = sc.nextDouble();
+        }
+
+        System.out.println("Alunos aprovados:");
+
+        for (int i=0; i<n; i++) {
+            double average = (score1[i] + score2[i]) / 2;
+            if (average >= 6) {
+                System.out.println(names[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
