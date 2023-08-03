260ea2b3e2a52465540ecbd00555448e84afac77
arthurNdev <arthurnina.dev@gmail.com>
2023-08-03 15:16:12 -0300

    Estudos Java

--- src/estudos/Basics7.java
+++ src/estudos/Basics7.java
@@ -1,41 +1,25 @@
 package estudos;
 
+import java.util.Scanner;
 public class Basics7 {
-
     public static void main(String[] args) {
-
-        String original = "abc de FGHIJ ABC abc DEFG ";
-
-        String s01 = original.toLowerCase();
-        String s02 = original.toUpperCase();
-        String s03 = original.trim();
-        String s04 = original.substring(2);
-        String s05 = original.substring(2, 9);
-        String s06 = original.replace('a', 'x');
-        String s07 = original.replace("abc", "xy");
-
-        int i = original.indexOf("bc");
-        int j = original.lastIndexOf("bc");
-
-        System.out.println("Original: -" + original + "-");
-        System.out.println("toLowerCase: -" + s01 + "-");
-        System.out.println("toUpperCase: -" + s02 + "-");
-        System.out.println("trim: -" + s03 + "-");
-        System.out.println("substring(2): -" + s04 + "-");
-        System.out.println("substring(2, 9): -" + s05 + "-");
-        System.out.println("replace('a', 'x'): -" + s06 + "-");
-        System.out.println("replace('abc', 'xy'): -" + s07 + "-");
-        System.out.println("Index of 'bc': " + i);
-        System.out.println("Last index of 'bc': " + j);
-
-        String s = "potato apple lemon orange";
-
-        String[] vect = s.split(" ");
-
-        System.out.println(vect[0]);
-        System.out.println(vect[1]);
-        System.out.println(vect[2]);
-        System.out.println(vect[3]);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int higher = max(a, b, c);
+        showResult(higher);
+        sc.close();
+    }
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        } else aux = Math.max(y, z);
+        return aux;
+    }
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
     }
 }
